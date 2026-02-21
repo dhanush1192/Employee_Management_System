478ef8c32a49f63bccae51ee1308096f071ffeb5
unknown <dhanushbonthala85@gmail.com>
2026-02-21 17:03:29 +0530

    Update backend/EmployeeController

--- backend/src/main/java/com/Balu/www/controller/EmployeeController.java
+++ backend/src/main/java/com/Balu/www/controller/EmployeeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Balu.www.model.Employee;
 import com.Balu.www.service.EmployeeService;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://employee-management-system-ycry.onrender.com/")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController 
